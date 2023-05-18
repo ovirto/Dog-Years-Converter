f32c8e52f613234b8747a44f7d9bf28f85677610
Ozie Virto <122977870+ovirto@users.noreply.github.com>
2023-05-18 03:33:32 -0500

    Add files via upload

---
+++ Dog Years Calculator/Dog Years Calculator/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Dog_Years_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.btnCalculation = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.err1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.err1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(126, 43);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(191, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Dog Years Converter";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(240, 116);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 4;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblAge.Location = new System.Drawing.Point(92, 116);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblAge.Size = new System.Drawing.Size(142, 20);
+            this.lblAge.TabIndex = 5;
+            this.lblAge.Text = "Your Age in Years:";
+            // 
+            // btnCalculation
+            // 
+            this.btnCalculation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnCalculation.Location = new System.Drawing.Point(56, 164);
+            this.btnCalculation.Name = "btnCalculation";
+            this.btnCalculation.Size = new System.Drawing.Size(139, 46);
+            this.btnCalculation.TabIndex = 6;
+            this.btnCalculation.Text = "Calculate";
+            this.btnCalculation.UseVisualStyleBackColor = true;
+            this.btnCalculation.Click += new System.EventHandler(this.btnCalculation_Click);
+            this.btnCalculation.Enter += new System.EventHandler(this.btnCalculation_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lblResult.Location = new System.Drawing.Point(0, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Padding = new System.Windows.Forms.Padding(0, 75, 0, 0);
+            this.lblResult.Size = new System.Drawing.Size(443, 450);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnReset.Location = new System.Drawing.Point(240, 164);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(139, 46);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+            this.lblName.Location = new System.Drawing.Point(1, 428);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(79, 13);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "Ozie Virto 2023";
+            // 
+            // err1
+            // 
+            this.err1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 450);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculation);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblResult);
+            this.Name = "Form1";
+            this.Text = "Dog Years Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.err1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Button btnCalculation;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.ErrorProvider err1;
+    }
+}
+
---
+++ Dog Years Calculator/Dog Years Calculator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dog_Years_Calculator
+{
+    public partial class Form1 : Form
+    {
+        int years;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblResult.Enabled = false;
+        }
+
+        private void btnCalculation_Click(object sender, EventArgs e)
+        {
+            lblResult.Enabled = true;
+            lblResult.Text = "This is now visable";
+            years = int.Parse(txtAge.Text);
+            lblResult.Text = "Your age in dog years is: " +  (years * 7).ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblResult.Enabled = false;
+            lblResult.Text = "";
+            years = 0;
+            txtAge.Text = string.Empty;
+        }
+    }
+}
